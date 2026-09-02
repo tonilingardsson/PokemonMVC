644873adbe7820a7822169951deb8e6e7776e3dc
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-09-02 14:00:46 +0200

    Created PokemonMVC.Models namespace with PokemonListResponse class.

---
+++ PokemonMVC/Models/PokemonListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonMVC.Models
+{
+    public class PokemonListResponse
+    {
+        public int Count { get; set; }
+        public string? Next { get; set; }
+        public string? Previous { get; set; }
+        public List<NamedApiResource> Results { get; set; } = new();
+    }
+}
