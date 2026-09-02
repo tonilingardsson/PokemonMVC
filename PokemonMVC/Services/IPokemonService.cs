3813fba3ded0b281e4921a65328431d10fad51cb
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-09-02 12:08:31 +0200

    Added IPokemonService in PokemonMVC.Services namespace.

---
+++ PokemonMVC/Services/IPokemonService.cs
@@ -0,0 +1,11 @@
+﻿using PokemonMVC.Models;
+
+namespace PokemonMVC.Services
+{
+    public interface IPokemonService
+    {
+
+        Task<PokemonListResponse?> GetPokemonListAsync(int limit = 20);
+        Task<PokemonDetails?> GetPokemonDetailsAsync(string name);
+    }
+}
