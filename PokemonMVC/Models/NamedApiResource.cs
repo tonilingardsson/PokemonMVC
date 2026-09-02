c465d85390c64bd8c7d91b6dcdf8e8ddc1a2ca23
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-09-02 13:51:03 +0200

    Created PokemonMVC.Models namespace and added NamedApiResource class with Name and Url string properties initialized to empty strings.

---
+++ PokemonMVC/Models/NamedApiResource.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonMVC.Models
+{
+    public class NamedApiResource
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+    }
+}
