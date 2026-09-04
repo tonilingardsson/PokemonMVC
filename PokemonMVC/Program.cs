814e4aa1994cbcda5d4906babebf87a1061fe768
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-09-04 07:37:08 +0200

    Refactor to minimal hosting; improve PokemonService

--- PokemonMVC/Program.cs
+++ PokemonMVC/Program.cs
@@ -1,6 +1,6 @@
 using PokemonMVC.Services;
 
-var builder = new WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Adding services to the container.
 // Enables MVC controller and Razor views
@@ -10,39 +10,27 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 // Tells dependency injection to provide an instance of PokemonService whenever IPokemonService is requested.
 builder.Services.AddScoped<IPokemonService, PokemonService>(); // PokemonService yet to come
-namespace PokemonMVC
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container. 
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
